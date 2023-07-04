a10bfde9543ad4be96e45cf77dc7095fbc5cce0d
Matous Melecky <melecmat@fel.cvut.cz>
2023-07-04 16:30:25 +0200

    change split_video paths

--- split_video.py
+++ split_video.py
@@ -6,14 +6,14 @@ import argparse
 parser = argparse.ArgumentParser(description="Extract every second frame of videos.")
 parser.add_argument("--video_dir", default="2022-10-06T16-34-42")
 parser.add_argument("--video_names", nargs='+')
-parser.add_argument("--dest", default="images")
 args = parser.parse_args()
 
 
 video_paths = [os.path.join(args.video_dir, video_name) for video_name in args.video_names]
 print(video_paths)
-if not os.path.exists(args.dest):
-    os.makedirs(args.dest)
+image_folder = os.path.join(args.video_dir, "images")
+if not os.path.exists(image_folder):
+    os.makedirs(image_folder)
 
 for frame_id, frame in enumerate(read_videos(video_paths)):
-    cv2.imwrite(os.path.join(args.dest, f"image_{frame_id}.jpg"), frame)
+    cv2.imwrite(os.path.join(image_folder, f"image_{frame_id}.jpg"), frame)
